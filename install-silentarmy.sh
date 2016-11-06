1fa626c112a039b5c5eb37c5f16fca6059398e4d
zekaf <jose.faisca@gmail.com>
2016-11-06 03:09:48 +0000

    Update install-silentarmy.sh

--- install-silentarmy.sh
+++ install-silentarmy.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# Download the AMD APP SDK (as of 27 Oct 2016, the latest version is 3.0) from
+# http://developer.amd.com/tools-and-sdks/opencl-zone/amd-accelerated-parallel-processing-app-sdk/
+# and extract it to /opt directory:
+
 # variables
-AMD_APP_SDK_SH="AMD-APP-SDK-v3.0.130.136-GA-linux64.sh" # URN
+AMD_APP_SDK="AMD-APP-SDKInstaller-v3.0.130.136-GA-linux64.tar.bz2"
+AMD_APP_SDK_SH="AMD-APP-SDK-v3.0.130.136-GA-linux64.sh"
 AMD_APP_SDK_VERSION="3.0"
-URL1="" # URL to AMD-APP-SDK installation script 
 AMD_GPU_PRO="amdgpu-pro-16.40-348864.tar.xz"
-URL2="https://www2.ati.com/drivers/linux/ubuntu"
+URL="https://www2.ati.com/drivers/linux/ubuntu"
 
 # install AMD APP SDK
 function installAMD-APP-SDK(){
@@ -13,13 +17,17 @@ function installAMD-APP-SDK(){
   	#OPENCL="ocl-icd-opencl-dev xserver-xorg-video-ati"
   	#sudo apt-get -y purge $OPENCL
   if 	[ ! -d "/opt/AMDAPPSDK-$AMD_APP_SDK_VERSION" ]; then
-  		[ -f ./URL1.txt ] && URL=$(cat ./URL1.txt)
-  		[ -f ./$AMD_APP_SDK_SH ] || curl -O -k "$URL1/$AMD_APP_SDK_SH"
-		  sudo ./$AMD_APP_SDK_SH
+  		cd /opt
+		tar -xvf $AMD_APP_SDK
+		sudo ./$AMD_APP_SDK_SH
   		ln -s /opt/AMDAPPSDK-$AMD_APP_SDK_VERSION /opt/AMDAPP
   		ln -s /opt/AMDAPP/include/CL /usr/include
   		ln -s /opt/AMDAPP/lib/x86_64/* /usr/lib/
   		ldconfig
+  else
+  		echo "/opt/AMDAPPSDK-$AMD_APP_SDK_VERSION not found"
+		echo "Download the AMD APP SDK and extract it to /opt directory"
+		exit 1
   fi
 }
 
@@ -27,15 +35,15 @@ function installAMD-APP-SDK(){
 function installAMD-GPU-PRO(){
   if 	[ ! -d "/tmp/$AMD_GPU_PRO" ]; then
       		cd /tmp 
-  		[ -f ./$AMD_GPU_PRO ] || curl -O -k "$URL2/$AMD_GPU_PRO"
-      		tar -Jxvf $AMD_GPU_PRO
+  		[ -f ./$AMD_GPU_PRO ] || curl -O -k "$URL/$AMD_GPU_PRO"
+      		tar -xvf $AMD_GPU_PRO
 		amdgpu-pro-driver/amdgpu-pro-install
+		rm tmp/$AMD_GPU_PRO
   fi
 }
 
-cd $HOME
 
-# dependencies
+# install dependencies
 sudo apt-get install git ocl-icd-opencl-dev libsodium-dev 
 
 # install AMD APP SDK
@@ -51,6 +59,8 @@ installAMD-GPU-PRO
 # stack or inform that there are no packages found
 dpkg -l amdgpu-pro
 
+cd $HOME
+
 # install silentarmy
 git clone https://github.com/mbevand/silentarmy.git
 cd silentarmy
@@ -59,6 +69,5 @@ make
 # add user to the video group 
 sudo usermod -a -G video $LOGNAME
 
-echo "You will need to log out and in again.
 echo "You will need to restart your machine to launch using the new graphics stack.
 shutdown -r -v +1
